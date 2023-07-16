fad283b2c494a47e42d944472b0aad1170c4d3c7
DevxPi <macro.devxpi@gmail.com>
2023-07-16 22:43:21 +0800

    add rand dependency and updatecode for generate random nubmer

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,12 @@
 use std::io; // To obtain user input and then print the result as output
+use rand::Rng; // rng stands for `random number generator`
 
 fn main(){
     println!("Guess the number!");
+
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
